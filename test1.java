5d76d898460c9a8ef01ab21d528b280f6edcab53
Mary-Eskandari <maryameskandari@email.arizona.edu>
2021-03-26 14:42:01 -0700

    New
    sun_beach_vision

---
+++ test1.java
@@ -0,0 +1,13 @@
+
+public class test1 {
+	boolean playButton = true;
+	public static void main(String[] args) {
+		int bankBalance = 500;
+		bankBalance = bankBalance +250;
+		bankBalance = bankBalance -100;
+		System.out.println(bankBalance);
+		
+		
+	}
+	
+}
